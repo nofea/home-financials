4b50144df15e87643adbddff2ce280ff4e64723b
nofea <nofearsheenhere@gmail.com>
2025-11-04 10:13:43 +0530

    Added the UIManager abstract class

---
+++ src/ui_manager.cpp
@@ -0,0 +1,37 @@
+#include "ui_manager.hpp"
+#include <iostream>
+
+UIManager::UIManager()
+{
+}
+
+UIManager::~UIManager()
+{
+}
+
+void UIManager::showError(commons::Result res) const
+{
+	if (res == commons::Result::Ok) 
+    {
+		return; // nothing to show for success
+	}
+
+	std::cerr << errorMessage(res) << std::endl;
+}
+
+std::string UIManager::errorMessage(commons::Result res)
+{
+	switch (res) 
+    {
+		case commons::Result::Ok:
+			return "Success.";
+		case commons::Result::InvalidInput:
+			return "Invalid input: please check the data you provided and try again.";
+		case commons::Result::NotFound:
+			return "Not found: the requested family/member does not exist.";
+		case commons::Result::DbError:
+			return "Internal error: data storage operation failed. Try again or contact support.";
+		default:
+			return "An unknown error occurred.";
+	}
+}
